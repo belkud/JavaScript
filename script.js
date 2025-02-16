089421f8ffcd24205217411b31717c1d936ce510
belkud <vlad-belkud@yandex.ru>
2025-02-16 20:58:02 +0300

    newInfo

--- script.js
+++ script.js
@@ -1,221 +1,90 @@
+ 
 
 
 
-//! https://nodejs.org/en - ссылка на скачивание nodeJS
 
-// через пуск зайти в PowerShell (нажимаем правой кнопкой мыши -> запуск от имени администратора)
-// Get-ExecutionPolicy
-// Set-ExecutionPolicy unrestricted
-// подтвердить Y
 
-//! npm create vite
+var x = 10
+console.log(x)
+// var - устаревшие названия переменных (лучше их не писать)
+// let - нельзя чтобы две переменные были под одним названием, но мы можем менять их значения
+// const - константа, значения неизменны
 
+var x = 12
+console.log(x)
 
 
-console.log(333 +123);
 
-let pol = 'vova'
 
-let pol2 = pol.split('').reverse().join('')
-console.log(pol2);
-if (pol == pol2) {
-    console.log(`значение ${pol} -  поллиндром`);
-} else {
-    console.log(`значение ${pol} -  не поллиндром`);
-}
+const z = 25
 
+console.log(z);
 
+let y = 6
+let y2 = 3
 
+console.log(y+y2)
+console.log(y-y2)
+console.log(y*y2)
+console.log(y/y2)
+console.log(y**2) // возведение числа в степень
+console.log(y%y2) // остаток от деления 
 
-let array = [10, 11, 12, 13, 14, 15]
-let array2 = [1, 1, 2, 3, 4, 5]
 
+let whiteColorWithBlackColor
+let white_color_with_black_color
 
-function summer(p) {
-    let summ = 0
-    for (let i = 0; i < p.length; i++) {   
-        summ+=p[i]
-    }
-    console.log(summ);
-}
-summer(array)
+let a = 5
+console.log(typeof a)
 
-summer(array2)
+let b = 'abc'
+console.log(typeof b)
 
-// Написать функцию, которая реализует работу оператора %. 
-// Функция принимает 2 параметра и возвращает остаток от 
-// деления первого параметра на второй. В функции исполь
-// зовать только + - * /, оператор % не использовать. 
+let c
+console.log(typeof c)
 
+let d = 'abc' * 5
+console.log(d)
 
-function balance (a, b) {
-    if(a>b) {
-        console.log(Math.round(((a/b)-parseInt(a/b))*b));
-        
-    } else {
-        console.log('Делитель больше делимого числа');
-        
-    }
-}
+console.log(y==y2); //'>' '<' '>=' '<=' '==' сравнение переменных (boolean)
+ 
+// let surname = prompt('Введите ваше имя')
+// console.log(surname)
 
-balance(15, 9)
+// let age = alert ('Ваш возраст')
 
 
-let text = document.querySelector('#text')
 
+// let number1 = +prompt('Введите первое число')
+// let number2 = +prompt('Введите второе число')
 
+// alert( 'сумма чисел = ' + (number1 + number2))
 
-let b = 2
-console.log(b);
 
-console.log(b=5);
+// console.log(number1 + number2)
 
-const n = 3
-console.log(n);
+let j = 5554356547568
+console.log('переменная j = ' + j)
 
-// console.log(n=8)
 
-const dayOfWeek = 'Четверг'
-console.log(dayOfWeek);
-console.log(typeof dayOfWeek);
+// Модуль 1.
 
-const day_of_week = b + n
-console.log(day_of_week);
+// Задание 1. Запросите у пользователя его имя и выведите в ответ:
+// «Привет, его имя!».
 
-// let n1 = Number(prompt('введите 1-е число', 'первое число'))
 
-// let n2 = +prompt('введите 2-е число', 'второе число')
-// alert('общая сумма чисел = ' + (n1+n2))
-// console.log(n1+n2);
 
-let num3 = 555
-console.log(typeof num3);
 
-let num4 = '555'
-console.log(typeof num4);
 
-let numBothNumbers = num3 + num4
-console.log(typeof Number(numBothNumbers));
+// Задание 2. Запросите у пользователя год его рождения, посчитайте,
+// сколько ему лет и выведите результат. Текущий год укажите
+// в коде как константу
 
-let numBothNumbers2 =  parseFloat(numBothNumbers)
-// let numBothNumbers2 = numBothNumbers * 1
-console.log(numBothNumbers2);
 
-let hours = 10
-let minutes = 15
-let time = 24*60 - hours*60 - minutes
-console.log('время до конца дня: ')
-console.log((23-hours) + ' часов' + ' и ' + (60 - minutes) + ' минут');
+// let side = +prompt('Введите сторону квадрата')
+// console.log(side*4)
 
 
-let memory = 10000
-console.log(memory/820 - (memory%820)/820 );
-console.log(Math.floor(memory/820));
+// промпт
 
-let n2 = 15
-console.log(n%2==0);
-
-
-
-
-
-//! мини календари
-
-let todays = new Date
-
-let today = document.querySelector('#today')
-today.innerHTML == todays.getDay()
-
-switch (todays.getDay()) {
-    case 0:
-        today.innerHTML = 'Воскресенье'
-        break;
-    case 1:
-        today.innerHTML = 'Понедельник'
-        break;
-    case 2:
-        today.innerHTML = 'Вторник'
-        break;
-    case 3:
-        today.innerHTML = 'Среда'
-        break;
-    case 4:
-        today.innerHTML = 'Четверг'
-        break;
-    case 5:
-        today.innerHTML = 'Пятница'
-        break;
-    case 6:
-        today.innerHTML = 'Суббота'
-        break;    
-    default:
-        today.innerHTML = 'Неверный диапазон'
-        break;
-    }
-    
-
-let days = document.querySelector('#days')
-if (todays.getDay()==0) {
-    days.children[todays.getDay()+7].classList.add('newClass')
-}
-
-days.children[todays.getDay()].classList.add('newClass') 
-
-
-console.log(window.screen);
-console.log(todays.getDay());
-
-console.log(days.nextElementSibling);
-console.log(days.baseURI);
-
-//! мини календари, завершение
-
-
-
-
-
-
-
-
-
-
-
-
-
-let info = window.location.search
-console.log(info);
-
-let extra_info = info.split('&')
-console.log(extra_info);
-
-for (let i = 0; i < extra_info.length; i++) {
-    let inf = extra_info[i].split('=')
-    text.innerHTML += inf[1] + '\n'
-}
-
-
-// let use = prompt('Введите логин')
-// let password = prompt('Введите пароль')
-// if (use=='admin' || password=='admin') {
-//     console.log('пользователь/пароль совпали');
-// } else {
-//     console.log('пользователь/пароль неверны');
-// }
-
-
-let obj = [5, 7, 'abc']
-console.log(typeof obj);
-
-
-
-
-console.log(window.location);
-console.log(window.history);
-console.log(window.navigator);
-console.log(window.screen);
-
-let u = 6
-console.log(++u);
-
-
-
+console.log('вам '+ (2025 - 'год рождения пользователя'))
