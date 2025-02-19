f0e255cbca8c28a49c0128e0859c8252baee4bab
belkud <vlad-belkud@yandex.ru>
2025-02-19 21:52:08 +0300

    new_tasks

--- src/main.ts
+++ src/main.ts
@@ -85,6 +85,11 @@ console.log(Boolean(false));
 // Значение true имеют все остальные значения, а их бесконечное множество 
 // (буквы, цифры, символы и так далее) 
 
+//! Диалоговые окна
+// let s = confirm('Хотите закрыть окно')
+// let s2 = alert('Хотите закрыть окно')
+// let s3 = prompt('Хотите закрыть окно')
+
 
 
 
@@ -101,6 +106,59 @@ console.log(Boolean(false));
 
 
 
+// 7. Запросите у пользователя текущее время (часы и минуты)
+// и выведите, сколько часов и минут осталось до следующего
+// дня.
+
+// let minutes = Number(prompt('Введите минуты'))
+// let hours = Number(prompt('Введите часы'))
+// console.log('До полуночи остало: ' + (23-hours) + ' часа и ' + (60-minutes) + ' Минут' );
+
+let dates = new Date
+console.log(dates);
+
+console.log('Осталось ' + (23 - dates.getHours()) + ' часа и ' + (60 - dates.getMinutes()) + ' минут' );
+
+
+// 1.Условное ветвление: if
+let age = 28
+// let age = +prompt('Сколько вам лет?')
+
+if (age < 18) {
+    console.log('Пользователь  несовершеннолетний');
+} else {
+    console.log('Введите цифры');
+}
+
+
+// 2. Тернарный оператор
+
+
+
+// 5<3 ? console.log('значение верно') : console.log('Значение не верно')
+
+let x1 = 10
+let x2 = 51
+
+x1<x2 ? console.log('значение верно') : console.log('Значение не верно')
+
+
+if (x1>x2) {
+    console.log('значение не верно 222')
+} 
+if (x1<x2){
+    console.log('Значение верно 222')
+}
+
+// let age2:any = prompt('Возраст?');
+
+// let message = (age2 < 0) ? 'У вас отрицательный возраст' :
+//   (age2 < 3) ? 'Здравствуй, малыш!' :
+//   (age2 < 18) ? 'Привет!' :
+//   (age2 < 100) ? 'Здравствуйте!' :
+//   'Какой необычный возраст!';
+
+// alert( message );
 
 
 
@@ -112,3 +170,119 @@ console.log(Boolean(false));
 
 
 
+let day = 3
+if (day==1) {
+    console.log('Понедельник');
+} else if (day==2) {
+    console.log('Вторник');
+} else if (day==3) {
+    console.log('Среда');
+} else if (day==4) {
+    console.log('Четверг');
+} else if (day==5) {
+    console.log('Пятница');
+} else if (day==6) {
+    console.log('Суббота');
+} else if (day==7){
+    console.log('Воскресенье')
+} else {
+    console.log('Такого дня недели не существует')
+}
+
+
+
+
+
+// switch (day) {
+//     case 1:
+//         console.log('Понедельник');
+//         break;
+//     case 2:
+//         console.log('Вторник');
+//         break;
+//     case 3:
+//         console.log('Среда');
+//         break;
+//     case 4:
+//         console.log('Четверг');
+//         break;
+//     case 5:
+//         console.log('Пятница');
+//         break;
+//     case 6:
+//         console.log('Суббота');
+//         break;
+//     case 7:
+//         console.log('Воскресенье');
+//         break;
+//     default:
+//         console.log('Такого дня недели не существует')
+//         break;
+// }
+
+
+// let nameJS = prompt('Какое «официальное» название JavaScript?') 
+
+// if (nameJS=='ECMAScript') {
+// console.log("Ответ правильный");
+// alert("Ответ правильный");
+// } else {
+//     console.log("Ответ НЕ правильный");
+// }
+
+
+// Используя конструкцию if..else, напишите код, который получает число через prompt, 
+// а затем выводит в alert:
+
+// 1, если значение больше нуля,
+// -1, если значение меньше нуля,
+// 0, если значение равно нулю.
+// Предполагается, что пользователь вводит только числа.
+let num = 12
+if(num>0) {
+    console.log('1')
+} else if (num<0) {
+    console.log('-1')
+} else {
+    console.log('0')
+}
+
+let a1 = 1
+let b1 = 5
+let result = (a1 + b1 < 4) 
+
+result ? console.log('Мало') : console.log('Много');
+
+
+
+let f = 2000
+if (f%400==0) {
+    console.log('год високосный');
+} else if (f%100==0) {
+    console.log('год не вискосный');
+} else if (f%4==0) {
+    console.log('год високосный');
+} else {
+    console.log('год не вискосный');
+}
+console.log(1900/4);
+
+
+let rub = 1000
+if (rub>=200 && rub<300) {
+    console.log(rub - rub*0.03);
+} else if (rub>=300 && rub<500) {
+    console.log(rub - rub*0.05);
+} else if (rub>=500) {
+    console.log(rub - rub*0.07);
+} else {
+    console.log('Данные не корректны');
+}
+
+
+let nums = -5
+if (nums>0) {
+    console.log(nums);
+} else {
+    console.log(-nums);
+}
