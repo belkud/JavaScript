5f9e99bc3b3c13738448a99af1cf94c1f47db55d
belkud <vlad-belkud@yandex.ru>
2025-03-05 21:43:17 +0300

    lesson

--- src/main.ts
+++ src/main.ts
@@ -253,14 +253,12 @@ for (let i = 0; i < 10; i++) {
 console.log('================');
 
 
-let i = 0
-console.log(++i);  
-console.log(i);
+// let i = 0
+// console.log(++i);  
+// console.log(i);
 
 
-i=0
-console.log(i--);
-console.log(i);
+
 
 let number = '1234567098'
 console.log(number.length);
@@ -274,8 +272,247 @@ for (let i = 0; i < array.length; i++) {
 }
 console.log(count);    
 
+console.clear()
+
+
+
+console.log ( - -105)
+
 
  
+
+// 7. Запросите у пользователя текущее время (часы и минуты) и выведите, сколько часов и минут осталось до следующего дня.
+// let minutes = Number(prompt('Введите минуты'))
+// let hours = Number(prompt('Введите часы'))
+
+let hours = 23
+let minutes = 5
+console.log('До полуночи остало: ' + (23-hours) + ' часа и ' + (60-minutes) + ' Минут' )
+
+
+// 1. for 
+// 2. while
+// 3. do while
+
+
+// for (let i = 0; i <= 10; i++) {
+//     console.log(i);
+    
+// }
+ 
+let num = 0
+
+
+
+// while (num<5) {
+//     console.log(num);
+//     num++  
+// }
+
+// do {
+//     console.log(num);
+//     num++
+// } while (num<5);
+
+
+
+// ■ Задания, в которых необходимо использовать WHILE.
+
+//! 1. Вывести # столько раз, сколько указал пользователь.
+
+let cell = 5
+let str = ''
+while (cell>0) {
+    cell--
+    str+='#'
+    console.log(str);
+}
+
+
+//! 2. Пользователь ввел число, а на экран вывелись все числа
+//! от введенного до 0.
+
+// let userNum = 5
+// let diapazone = ''
+// while (userNum>0) {
+//     diapazone += userNum
+//     console.log(diapazone);
+//     userNum-- 
+// }
+
+//! 3. Запросить число и степень. Возвести число в указанную
+//! степень и вывести результат.
+
+let x = 5
+let pow = 3
+
+while (pow>0) {
+    x**pow
+    console.log(x**pow);
+    pow--
+}
+console.clear()
+
+//! 4. Запросить 2 числа и найти все общие делители.
+
+// let num1 = 81
+// let num2 = 27
+// let devider = num1
+// while (devider>0) {
+//     if (num1%devider==0 && num2%devider==0) {
+//         console.log(devider);
+//     } 
+//     devider--
+// }
+
+//! 5. Посчитать факториал введенного пользователем числа.(от 0 до введеного числа)
+
+// let y = 5
+// let f = 1
+
+// while (y>0) {
+//     f*=y
+//     console.log(f);
+//     y--
+// }
+
+// Пошаговое выполнение итераций 1*5 5*4 20*3 60*2 120*1
+
+
+
+
+
+
+//! 3. Вывести все числа от 1 до 100, которые кратные указанному
+//! пользователем числу.
+
+// let n = 5
+
+// for (let i = 1; i < 100; i++) {
+//     if(i%n==0) {
+//         console.log(i);
+        
+//     }
+// }
+
+//! 4. Вывести каждый 4-й элемент из указанного пользователем
+//! диапазона. Пользователь указывает минимальное и максимальное значения диапазона. 
+
+
+let minNum = 25
+let maxNum = 80
+let string = ''
+
+
+for (let i = minNum; i < maxNum; i+=4 ) {
+    console.log(i);        
+}
+
+
+
+
+
+//! 5. Запросить число и проверить, простое ли оно. Простое
+//! число делится без остатка только на себя и на единицу.
+
+// 5   7    11 - на себя или на 1
+
+
+console.log('=======');
+
+// let y = 13
+// let acc = 0
+// for (let i = 2; i < y; i++) {
+//     if (y%i==0) {
+    //         acc++
+        
+//     } 
+    
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+let y = 25
+let acc = 0
+for (let i = 1; i <= y; i++) {
+    if (y % i == 0) {
+        acc++
+    }
+}
+console.log(acc > 2 ? 'число составное' : 'число простое')
+
+
+
+
+// let y = 25
+// let acc = 0
+// for (let i = 2; i < y; i++) {
+//     if (y % i == 0) {
+//         acc++
+//     }
+// }
+// console.log(acc > 0 ? 'число составное' : 'число простое')
+
+
+
+//! 4. Определить количество цифр в введенном числе
+
+let b = 12345
+console.log(b.toString().length);
+
+let b12 = '12345678'
+console.log(b12.length);
+
+
+for (let i = 10; i <= 20; i++) {
+    console.log(i);
+        
+}
+
+
+// 5. Запросить у пользователя 10 чисел и подсчитать, сколько
+// он ввел положительных, отрицательных и нулей. При этом
+// также посчитать, сколько четных и нечетных. Вывести
+// статистику на экран. Учтите, что достаточно одной переменной 
+// (не 10) для ввода чисел пользователем.
+ 
+
+
+
+// for (let i = 1; i <= 10; i++) {
+//   const num = Number(prompt("Введите число"));
+//   if (num > 0) {
+//     positive++;
+//   }
+//   if (num < 0) {
+//     negative++;
+//   }
+//   if (num == 0) {
+//     zero++;
+//   }
+//   if (num % 2 == 1) {
+//     odd++;
+//   }
+//   if (num % 2 == 0) {
+//     even++;
+//   }
+// }
+// alert(` Из введённых цифр — положительных: ${positive}, отрицательных: ${negative}, нулей: ${zero}, четных: ${even}, нечетных: ${odd}.`)
+
+
+
+
+
 
 
 
